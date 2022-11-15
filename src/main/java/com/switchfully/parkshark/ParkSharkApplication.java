340f7f0d1e97003f7239464ab86265848dd2964e
fmkarakus <fatihkarakus@hotmail.com>
2022-11-15 09:57:34 +0100

    initial

---
+++ src/main/java/com/switchfully/parkshark/ParkSharkApplication.java
@@ -0,0 +1,13 @@
+package com.switchfully.parkshark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkSharkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParkSharkApplication.class, args);
+    }
+
+}
