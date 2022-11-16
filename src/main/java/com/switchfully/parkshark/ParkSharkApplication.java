9354a1c4bc805e29acb4936fe3fc09c26d867f11
dominiquevaneyken <dominique.van.eyken@telenet.be>
2022-11-16 09:25:43 +0100

    securityConfig bug fix

--- src/main/java/com/switchfully/parkshark/ParkSharkApplication.java
+++ src/main/java/com/switchfully/parkshark/ParkSharkApplication.java
@@ -1,7 +1,15 @@
 package com.switchfully.parkshark;
 
+import org.jboss.resteasy.client.jaxrs.ResteasyClientBuilder;
+import org.keycloak.OAuth2Constants;
+import org.keycloak.adapters.KeycloakConfigResolver;
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.keycloak.admin.client.Keycloak;
+import org.keycloak.admin.client.KeycloakBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ParkSharkApplication {
@@ -10,4 +18,24 @@ public class ParkSharkApplication {
         SpringApplication.run(ParkSharkApplication.class, args);
     }
 
+    @Bean
+    public Keycloak keycloak(@Value("${master.keycloak.username}") String adminUsername, @Value("${master.keycloak.password}") String adminPassword) {
+        return KeycloakBuilder.builder()
+                .serverUrl("https://keycloak.switchfully.com/auth")
+                .grantType(OAuth2Constants.PASSWORD)
+                .realm("parkShark-babyshark")
+                .clientId("parkShark")
+                .username(adminUsername)
+                .password(adminPassword)
+                .resteasyClient(
+                        new ResteasyClientBuilder()
+                                .connectionPoolSize(10).build()
+                )
+                .build();
+    }
+
+    @Bean
+    public KeycloakConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
 }
--- src/main/java/com/switchfully/parkshark/security/SecurityConfig.java
+++ src/main/java/com/switchfully/parkshark/security/SecurityConfig.java
@@ -1,7 +1,5 @@
 package com.switchfully.parkshark.security;
 
-import org.keycloak.adapters.KeycloakConfigResolver;
-import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
 import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
 import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
 import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
@@ -49,11 +47,6 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
 
-    @Bean
-    public KeycloakConfigResolver KeycloakConfigResolver() {
-        return new KeycloakSpringBootConfigResolver();
-    }
-
     /**
      * Spring security configuration
      * This is the default security configuration and shouldn't be changed
@@ -85,7 +78,6 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
         corsConfig.addAllowedMethod("PATCH");
         corsConfig.addAllowedMethod("DELETE");
 
-
         UrlBasedCorsConfigurationSource source =
                 new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", corsConfig);
@@ -100,6 +92,4 @@ class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web.ignoring().antMatchers("/public-hello-world");
     }
-
-
 }
