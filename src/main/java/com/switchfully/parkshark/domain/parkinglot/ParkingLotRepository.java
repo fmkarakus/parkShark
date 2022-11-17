f060d82bc8826be1e6758531d6c2781be9c275ac
Jordy Vanneste <jordyvanneste@hotmail.com>
2022-11-17 09:02:37 +0100

    removed some unused imports

--- src/main/java/com/switchfully/parkshark/domain/parkinglot/ParkingLotRepository.java
+++ src/main/java/com/switchfully/parkshark/domain/parkinglot/ParkingLotRepository.java
@@ -3,6 +3,9 @@ package com.switchfully.parkshark.domain.parkinglot;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ParkingLotRepository extends JpaRepository<ParkingLot,Long> {
+
 }
